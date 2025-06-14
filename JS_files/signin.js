215031fdaad916d761c94aaf1c47b01d12ea578d
XCticon <Azusagawa.sakuta.m@gmail.com>
2025-06-15 02:49:16 +0300

    desgining placeholder

--- JS_files/signin.js
+++ JS_files/signin.js
@@ -25,40 +25,48 @@ if(input__username) {
         username__label.style.transform = "translate3d(0, -1.75em, 0)";
         username__label.style.color = "hsl(var(--bg))";
         username__label.style.backgroundColor = "hsl(var(--muted))";
+        input__username.style.setProperty("--controlopacity", "1");
     });
     input__username.addEventListener("blur", function() {
         username__label.style.transform = "translate3d(0, 0, 0)";
         username__label.style.backgroundColor = "hsl(var(--muted))";
+        input__username.style.setProperty("--controlopacity", "0");
         username__label.style.color = "";
     });
     input__email.addEventListener("focus", function() {
         email__label.style.transform = "translate3d(0, -1.75em, 0)";
         email__label.style.color = "hsl(var(--bg))";
         email__label.style.backgroundColor = "hsl(var(--muted))";
+        input__email.style.setProperty("--controlopacity", "1");
     });
     input__email.addEventListener("blur", function() {
         email__label.style.transform = "translate3d(0, 0, 0)";
         email__label.style.backgroundColor = "hsl(var(--muted))";
         email__label.style.color = "";
+        input__email.style.setProperty("--controlopacity", "0");
     });
     input__phone.addEventListener("focus", function() {
         phone__label.style.transform = "translate3d(0, -1.75em, 0)";
         phone__label.style.color = "hsl(var(--bg))";
         phone__label.style.backgroundColor = "hsl(var(--muted))";
+        input__phone.style.setProperty("--controlopacity", "1");
     });
     input__phone.addEventListener("blur", function() {
         phone__label.style.transform = "translate3d(0, 0, 0)";
         phone__label.style.backgroundColor = "hsl(var(--muted))";
         phone__label.style.color = "";
+        input__phone.style.setProperty("--controlopacity", "0");
     });
     input__password.addEventListener("focus", function() {
         password__label.style.transform = "translate3d(0, -1.75em, 0)";
         password__label.style.color = "hsl(var(--bg))";
         password__label.style.backgroundColor = "hsl(var(--muted))";
+        input__password.style.setProperty("--controlopacity", "1");
     });
     input__password.addEventListener("blur", function() {
         password__label.style.transform = "translate3d(0, 0, 0)";
         password__label.style.backgroundColor = "hsl(var(--muted))";
         password__label.style.color = "";
+        input__password.style.setProperty("--controlopacity", "0");
     });
 }
