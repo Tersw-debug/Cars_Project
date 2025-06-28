db838aaad34ed56997dae16e7931a4b542efcf70
XCticon <Azusagawa.sakuta.m@gmail.com>
2025-06-28 22:58:38 +0300

    added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require(`express`);
 const app = express();
-
-app.use(express.static("public"));
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res) =>{
     res.send('Welcome to my static!');
